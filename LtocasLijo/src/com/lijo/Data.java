630ed1508096029abd33c5bb5ec367c808ad8dc2
VijayViju <vijay1997may@gmail.com>
2019-09-24 13:02:18 +0530

    asda

--- LtocasLijo/src/com/lijo/Data.java
+++ LtocasLijo/src/com/lijo/Data.java
@@ -4,10 +4,9 @@ public class Data {
 	public static void main(String[] args) {
 		System.out.println("LtoCas Lijo.......");
 		System.out.println("i am aruna");
-		System.out.println("LtoCas....... hello");
-		System.out.println("LtoCas....... server");
-		System.out.println("this is new");
-		System.out.println("this is new one");
+		System.out.println("LtoCas....... hello");   
+		System.out.println("LtoCas....... server");  
+		System.out.println("LtoCas....... server");  
 
 
 	}
